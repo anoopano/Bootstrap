ee34323f61c2b0fdee97408d0b3683a6f080cf03
anoopano <anoopp998@gmail.com>
2020-08-25 00:37:29 +0530

    Add cards in dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './style.css'
+
+
+function Dashboard() {
+    return (
+        <div className="container-fluid">
+        <div className="row">
+                <div className="col">
+                    <div className="card cards">
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                        </div>
+                    </div>
+                </div>
+
+                
+                <div className="col">
+                <div className="card cards">
+                    <div className="card-body">
+                        <h5 className="card-title">Special title treatment</h5>
+                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                    </div>
+                </div></div>
+
+                <div className="col">
+                <div className="card cards">
+                    <div className="card-body">
+                        <h5 className="card-title">Special title treatment</h5>
+                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                    </div>
+                </div></div>
+
+                <div className="col">
+                <div className="card cards">
+                    <div className="card-body">
+                        <h5 className="card-title">Special title treatment</h5>
+                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                    </div>
+                </div></div>
+
+                </div>
+        </div>
+    )
+
+}
+
+export default Dashboard
+
